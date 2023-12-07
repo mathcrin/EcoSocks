71ea7f0f5d95058e33e28b4927bc38fbdf4f91a6
Theo Lorthioir <73892287+Taumicron@users.noreply.github.com>
2023-12-07 19:52:52 +0100

    Commit initial

---
+++ backend/src/main/java/fr/nuitdelinfo/EcoSocks/EcoSocksApplication.java
@@ -0,0 +1,13 @@
+package fr.nuitdelinfo.EcoSocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoSocksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/nuitdelinfo/EcoSocks/EcoSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nuitdelinfo.EcoSocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
