c26ac0d16777eed066ccb399439e6b470f3a858a
AurelieCh <aurelie.alan@outlook.fr>
2023-12-08 00:29:05 +0100

    {Edit} Jeu + section infos
    Ajout des cards et de la section tableau

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {CardComponent} from "./composants/card/card.component";
+import {TableauCardComponent} from "./composants/tableau-card/tableau-card.component";
+import {ConsommationComponent} from "./composants/consommation/consommation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CardComponent],
+    imports: [CommonModule, RouterOutlet, CardComponent, TableauCardComponent, ConsommationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
